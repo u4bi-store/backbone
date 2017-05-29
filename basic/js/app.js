7a96c2cce218746f4f41adf0ebec599c995bef4b
Yu Myungjae <u4bi@letsee.io>
2017-05-29 15:21:08 +0900

    백본 환경 초기화 및 메인 라우터 등록

---
+++ basic/js/app.js
@@ -0,0 +1,36 @@
+Router= Backbone.Router.extend({
+
+	initialize: function () {
+		console.log('route init');
+	},
+
+	routes: {
+		'humans': 'main'
+	},
+	
+    showView: function (view) {
+		if (this.currentView) this.currentView.remove();
+
+		this.currentView = view;
+		$('app').html(this.currentView.render()); /* <app> 태그를 기점으로 렌더링 */
+	},
+
+    main: function () {
+
+        console.log('route main 함수');
+
+		var human = new Human(),
+			init = new HumanInitView({ model: human });
+			
+		init.render();
+		
+        this.showView(humans.view);
+
+	}
+});
+
+var humans = new Humans(); 	/* 휴먼스 컬렉션 생성 */
+var router = new Router();  /* 라우팅 생성      */
+
+Backbone.history.start();   /* 라우팅에 대한 진행기록 시작 */
+router.navigate('humans');  /* humans를 기점으로 라우팅을 시작 */
